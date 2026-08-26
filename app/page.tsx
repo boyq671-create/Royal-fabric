4c9b3d5074a44c77b7be54b87956b6eeae603c8f
boyq671-create <boyq671@gmail.com>
2026-08-26 18:45:34 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Welcome to Royal Fabric 🛍️</h1>
-      <p>Aapki website successfully live ho chuki hai!</p>
-    </main>
-  )
-}
